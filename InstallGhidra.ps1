c4d1b8bb525620447784c9712aadf21d045e6362
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-02-12 18:18:09 +0100

    Add files via upload

--- InstallGhidra.ps1
+++ InstallGhidra.ps1
@@ -8,19 +8,20 @@ Write-Host "Attendee la fin de l'installation !" -ForegroundColor Magenta
 Write-Host "Installation dans : '$env:USERPROFILE\'" -ForegroundColor Yellow 
 Write-Host ""
 Write-Host ""
+Start-Sleep 5
 #---------------
 # Install Ghibra
 curl -O "Ghidra 10.2.3.zip" "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.2.3_build/ghidra_10.2.3_PUBLIC_20230208.zip"
 if ($? -eq $True) {
     Write-Host "[OK]" -ForegroundColor Green -NoNewline
     Write-Host "Telechargement de Ghibra !"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Telechargement de Ghibra"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Telechargement de Ghibra";}
 
 Expand-Archive -Path "Ghidra 10.2.3.zip" -DestinationPath "$env:USERPROFILE\"
 if ($? -eq $True) {
     Write-Host "[OK]" -ForegroundColor Green -NoNewline
     Write-Host "Decompression Ghibra !"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Decompression Ghibra"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Decompression Ghibra";}
 cd "$Back"
 rm -force "Ghibra 10.2.3.zip"
 cd "$env:USERPROFILE\"
@@ -31,8 +32,7 @@ cd "$Back"
 #---------------
 # Install Java
 java -version
-if ($? -eq $False)
-{
+if ($? -eq $False) {
     curl -O "JavaSetup8u361.exe" "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=247917_0ae14417abb444ebb02b9815e2103550"
     Start-Process "JavaSetup8u361.exe" -Wait
     if ($? -eq $False) {
@@ -42,20 +42,20 @@ if ($? -eq $False)
         exit
     }
     rm -force "JavaSetup8u361.exe"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Installation Java"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Installation Java";}
 #---------------
 # Install JDK
 curl -O "jdk-19_windows-x64_bin.msi" "https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.msi"
 if ($? -eq $True) {
     Write-Host "[OK]" -ForegroundColor Green -NoNewline
     Write-Host "Telechargement de JDK !"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Telechargement de JDK"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Telechargement de JDK";}
 
 Start-Process "jdk-19_windows-x64_bin.msi" -Wait
 if ($? -eq $True) {
     Write-Host "[OK]" -ForegroundColor Green -NoNewline
     Write-Host "Installation JDK !"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Installation JDK"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Installation JDK";}
 
 rm -force "jdk-19_windows-x64_bin.msi"
 #---------------
@@ -68,7 +68,7 @@ $Shortcut.Save()
 if ($? -eq $True) {
     Write-Host "[OK]" -ForegroundColor Green -NoNewline
     Write-Host "Racourci Ghidra cree !"
-} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Racourci Ghidra"}
+} else {Write-Host "[ERREUR]" -ForegroundColor Red -NoNewline; Write-Host "Racourci Ghidra";}
 
 Start-Sleep 5
 
